3a6006a5bc3486e7d1b25e0817e5d54e3c96f3e1
Ted Bisson <tedbisson@gmail.com>
2025-02-23 14:41:51 -0800

    Moved ring generation to the Disc class. Discs are flat, possibly with a hole, rings are cylinders without end caps.

--- Generators/Disc.cs
+++ Generators/Disc.cs
@@ -1,4 +1,5 @@
 ﻿using GeometryGenerator.Geometry;
+using System.Numerics;
 
 namespace GeometryGenerator.Generators
 {
@@ -29,12 +30,69 @@ namespace GeometryGenerator.Generators
         public Model Create(Dictionary<string, float> optionValues)
         {
             // Extract the required creation values.
-            int sectors = (int)optionValues["Stacks"];
-            int tracks = (int)optionValues["Slices"];
+            int sectors = (int)optionValues["Sectors"];
+            int tracks = (int)optionValues["Tracks"];
             float innerRadius = optionValues["InnerRadius"];
             float outerRadius = optionValues["OuterRadius"];
 
-            return new Model();
+            Mesh mesh = new Mesh("Ring");
+
+            float deltaTheta = MathF.Tau / sectors;
+            float deltaRadius = (outerRadius - innerRadius) / tracks;
+
+            // Create vertices around the ring.
+            float radius = innerRadius;
+            for (int track = 0; track <= tracks; ++track)
+            {
+                float theta = 0.0f;
+                for (int sector = 0; sector < sectors; ++sector)
+                {
+                    Vector3 v = new Vector3(
+                        radius * MathF.Sin(theta),
+                        radius * MathF.Cos(theta),
+                        0.0f);
+
+                    mesh.AddVertex(v);
+
+                    theta += deltaTheta;
+                }
+
+                radius += deltaRadius;
+            }
+
+            // Create faces.
+            for (int track = 0; track < tracks; ++track)
+            {
+                for (int sector = 0; sector < sectors - 1; ++sector)
+                {
+                    int a = track * sectors + sector;
+                    int b = a + 1;
+                    int c = a + sectors;
+                    int d = c + 1;
+
+                    mesh.AddFace(new Face(a, b, c));
+                    mesh.AddFace(new Face(a, c, b));
+
+                    mesh.AddFace(new Face(b, d, c));
+                    mesh.AddFace(new Face(b, c, d));
+                }
+
+                {
+                    int a = track * sectors + sectors - 1;
+                    int b = a + 1 - sectors;
+                    int c = a + sectors;
+                    int d = c + 1 - sectors;
+
+                    mesh.AddFace(new Face(a, b, c));
+                    mesh.AddFace(new Face(a, c, b));
+
+                    mesh.AddFace(new Face(b, d, c));
+                    mesh.AddFace(new Face(b, c, d));
+                }
+
+            }
+
+            return new Model(mesh);
         }
     }
 }
